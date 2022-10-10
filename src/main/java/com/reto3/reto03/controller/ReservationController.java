62b6466dacb3b5d70a878931691ed5bb6848aef4
nelsonsegura <durca45@outlook.com>
2022-10-10 15:19:41 -0500

    proyecto final probado

---
+++ src/main/java/com/reto3/reto03/controller/ReservationController.java
@@ -0,0 +1,31 @@
+package com.reto3.reto03.controller;
+
+import com.reto3.reto03.entities.Admin;
+import com.reto3.reto03.entities.Reservation;
+import com.reto3.reto03.service.AdminService;
+import com.reto3.reto03.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/Reservation")
+public class ReservationController {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @GetMapping("/all")
+    public List<Reservation> getAll(){
+        return reservationService.getAll();
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation save(@RequestBody Reservation p){
+        return reservationService.save(p);
+    }
+}
